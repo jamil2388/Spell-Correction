4d078371c8ec06996ababffa51bd14bd44b3f5d8
jamil2388 <ahmed491@uwindsor.ca>
2024-02-01 01:40:36 -0500

    trying multiprocessing

--- param.py
+++ param.py
@@ -1,9 +1,10 @@
 import os
 
 settings = {
-    'num_cpu_cores' : os.cpu_count() - 4,
+    # 'num_cpu_cores' : os.cpu_count() - 4,
+    'num_cpu_cores' : 2,
     'path_to_cache' : 'cache',
     'bb_groups_filename' : 'bb_groups.pkl',
-    'toy' : True,
-    'batched' : True,
+    'toy' : 1,      # 1 => True
+    'batched' : 1,  # 1 => True
 }
