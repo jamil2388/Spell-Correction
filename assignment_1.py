09e80abdebb096dcaf0a0d6841cb78607ef54021
jamil2388 <ahmed491@uwindsor.ca>
2024-02-08 09:29:08 -0500

    minor tweaks

--- assignment_1.py
+++ assignment_1.py
@@ -35,9 +35,9 @@ if __name__ == "__main__":
 
     # total timing of the entire run
     global_end_time = time.time()
-    global_total_time = global_end_time - global_start_time
+    global_total_time = (global_end_time - global_start_time) / 60
     print('\n---------------- Time Taken ---------------\n')
-    print(f'Total Time taken by the entire program : {(global_total_time / 60):.4f} minutes')
+    print(f'Total Time taken by the entire program : {global_total_time:.4f} minutes')
     print('\n---------------- |||||||||| ---------------\n')
 
     # print(f"\ns@k entries having 1 : \n{np.argwhere(s_at_k == 1).sum()}\n\n")
